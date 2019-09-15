32370b787ee9391020a9680b9b1a3fadde1e6eec
MykSto <hushokas@gmail.com>
2019-09-15 19:49:38 +0300

    [001]: created meme generator components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
+import * as $ from 'jquery';
+declare const myTest: any;
+import {FileSelectDirective, FileUploader} from 'ng2-file-upload';
+const uri = 'http://localhost:3000/file/upload';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  
+  public ngOnInit(){
+
+  }
+  
+// uploader:FileUploader = new FileUploader({url:uri});
+ 
+// attachmentList: any = [];
+
+// constructor(){
+//   this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any)=> {
+//     this.attachmentList.push(JSON.parse(response));
+//   }
+// }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemeGeneratorComponent } from './meme-generator/meme-generator.component';
+
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemeGeneratorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
